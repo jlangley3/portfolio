627c89d582c11ad7d8382b1d564c95a85552b094
jlangley3 <jlangleyiii@aol.com>
2020-09-11 18:22:29 -0400

    browser router working

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+
+class Home extends React.Component{
+
+}
+  
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Link, withRouter } from "react-router-dom";
+import { connect } from 'react-redux';
+
+
+class NavBar extends Component {
+  
+}
+
+
+
+
+
+
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(NavBar))
+
