7be90c74c5112bf8e78b29b9d824c2f9e0c85503
jlangley3 <jlangleyiii@aol.com>
2020-09-15 15:57:17 -0400

    added projects div

---
+++ src/components/Porjects.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link, withRouter } from "react-router-dom";
+import { connect } from 'react-redux';
+
+
+
+class Projects extends Component {
+    render() {
+        return ( 
+                <div></div>
+              )}       
+}
+
+
+
+
+
+
+
+export default Projects
