225c3e0485b4600edfacfd62056b2c2603b7f1d5
jlangley3 <jlangleyiii@aol.com>
2020-09-15 15:46:03 -0400

    working ob nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from "react-router-dom";
 import { connect } from 'react-redux';
+import '../styles/Nav.css';
 
 
 class NavBar extends Component {
     render() {
         return ( 
-            <div className="App">
-                This is My Nav Bar
-                
-            </div>
+               <ul>
+                    <li><a href="default.asp">Home</a></li>
+                    <li><a href="projects.asp">Projects</a></li>
+                    <li><a href="contact.asp">Contact</a></li>
+                    <li><a href="about.asp">About</a></li>
+                </ul>
               )}       
 }
 
