df98a91543fa98fe18ec9edc406c801ab995ffc4
jlangley3 <jlangleyiii@aol.com>
2020-09-29 14:59:06 -0400

    working on embedded youtube videos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from "react-router-dom";
 import { connect } from 'react-redux';
-
+import '../styles/Footer.css';
 
 
 class Projects extends Component {
     render() {
         return ( 
-                <div>Projects</div>
+                <div>Projects
+                   <iframe  src="https://www.youtube.com/embed/IL3Uz5mYQ6A"
+                     />
+                </div>
               )}       
 }
 
