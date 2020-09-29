fab0265b6e61e77fd5daf18814e1a548c3e597a8
jlangley3 <jlangleyiii@aol.com>
2020-09-29 19:54:12 -0400

    added all videos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,9 +7,28 @@ import { connect } from 'react-redux';
 class Projects extends Component {
     render() {
         return ( 
-                <div>Projects
+                <div>
                    <iframe width="655" height="500" 
                            src="https://www.youtube.com/embed/IL3Uz5mYQ6A?rel=0" 
+                           allow="accelerometer; gyroscope; picture-in-picture" 
+                           allowfullscreen
+                           frameborder="10312424"
+                           title='gift lister video'
+                    />
+                   <iframe width="655" height="500" 
+                           src="https://www.youtube.com/embed/2_enCjVPRtM?rel=0" 
+                           allow="accelerometer; gyroscope; picture-in-picture" 
+                           allowfullscreen
+                           title='Car Facts video'
+                    />
+                   <iframe width="655" height="500" 
+                           src="https://www.youtube.com/embed/fGRfg76xeHg?rel=0" 
+                           allow="accelerometer; gyroscope; picture-in-picture" 
+                           allowfullscreen
+                           title='gift lister video'
+                    />
+                   <iframe width="655" height="500" 
+                           src="https://www.youtube.com/embed/wYSYQiH26zk?rel=0" 
                            allow="accelerometer; gyroscope; picture-in-picture" 
                            allowfullscreen
                            title='gift lister video'
@@ -26,5 +45,5 @@ title='video'> */}
 
 
 
-
 export default Projects
+
