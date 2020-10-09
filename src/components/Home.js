23283ea4c2be7f8c14585bfc313b9529ac9775d5
jlangley3 <jlangleyiii@aol.com>
2020-10-09 15:21:00 -0400

    added styling to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import me from '../images/jessepic.jpg';
+import '../styles/Projects.css';
 
 class Home extends React.Component{
     render() {
         return ( 
             <div className="App">
-            Welcome to My Profile!
+               <h1>Welcome to My Protfolio!</h1>
                  <img src={me} alt="me"/>
             </div>
               )}       
