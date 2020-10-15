6a15b5a63a5c2b9fbbf8492e7043d6cb691932be
jlangley3 <jlangleyiii@aol.com>
2020-10-15 13:15:13 -0400

    Working on project link size

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,33 +15,35 @@ class Projects extends Component {
                            allowfullscreen
                            title='gift lister video'
                     />
-                   <p>Link to Front-end: <a href={"https://github.com/jlangley3/gift_list_frontend"}>Front-End</a> ||  Link to Back-end: <a href={"https://github.com/jlangley3/gift_list_backend"}>Back-End</a></p>
+                   <p>
+                        Link to Front-end: <a href={"https://github.com/jlangley3/gift_list_frontend"}>Front-End</a> 
+                         ||  
+                        Link to Back-end: <a href={"https://github.com/jlangley3/gift_list_backend"}>Back-End</a>
+                        </p>
                    <iframe width="655" height="500" 
                            src="https://www.youtube.com/embed/2_enCjVPRtM?rel=0" 
                            allow="accelerometer; gyroscope; picture-in-picture" 
                            allowfullscreen
                            title='Car Facts video'
                     />
-                    <p>Link to Front-end: <a href={"https://github.com/jlangley3/react_car_forum"}>Front-End</a> ||  Link to Back-end: <a href={"https://github.com/agdsarge/rails-car-forum"}>Back-End</a></p>
-                   <ul>
-                        <li>Link to Front-end: <a href={"https://github.com/jlangley3/react_car_forum"}>GitHub</a></li>
-                        <li>Link to Back-end: <a href={"https://github.com/agdsarge/rails-car-forum"}>GitHub</a></li>
-                   </ul>
+                    <p>
+                        Link to Front-end: <a href={"https://github.com/jlangley3/react_car_forum"}>Front-End</a>
+                         ||  
+                        Link to Back-end: <a href={"https://github.com/agdsarge/rails-car-forum"}>Back-End</a>
+                        </p>
+
                    <iframe width="655" height="500" 
                            src="https://www.youtube.com/embed/fGRfg76xeHg?rel=0" 
                            allow="accelerometer; gyroscope; picture-in-picture" 
                            allowfullscreen
                            title='Bootcamp video'
                     />
-                    <p>Link to Front-end: 
-                            <a href={"https://github.com/jlangley3/bootcamp_fitness-frontend"}>Front-End</a> 
-                            ||  Link to Back-end: 
-                            <a href={"hhttps://github.com/jlangley3/bootcamp-fitness-backend"}>Back-End</a>
+                    <p>
+                        Link to Front-end: <a href={"https://github.com/jlangley3/bootcamp_fitness-frontend"}>Front-End</a> 
+                         ||  
+                        Link to Back-end: <a href={"hhttps://github.com/jlangley3/bootcamp-fitness-backend"}>Back-End</a>
                     </p>
-                   <ul>
-                        <li>Link to Front-end: <a href={"https://github.com/jlangley3/bootcamp_fitness-frontend"}>GitHub</a></li>
-                        <li>Link to Back-end: <a href={"https://github.com/jlangley3/bootcamp-fitness-backend"}>GitHub</a></li>
-                   </ul>
+
                    <iframe width="655" height="500" 
                            src="https://www.youtube.com/embed/wYSYQiH26zk?rel=0" 
                            allow="accelerometer; gyroscope; picture-in-picture" 
@@ -49,9 +51,6 @@ class Projects extends Component {
                            title='Artist is Like video'
                     />
                     <p>Link to Code: <a href={"https://github.com/mertoz41/artist-is-like"}>GitHub</a></p>
-                   <ul>
-                        <li>Link to Code: <a href={"https://github.com/mertoz41/artist-is-like"}>GitHub</a></li>
-                   </ul>
                 </div>
               )}       
 }
