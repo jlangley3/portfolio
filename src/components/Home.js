cf1471bf03c98f5d6116b33ea48aecde078485b7
jlangley3 <jlangleyiii@aol.com>
2020-12-05 17:02:05 -0500

    Style Fixes

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ class Home extends React.Component{
     render() {
         return ( 
             <div className="App">
-               <h1>Welcome to My Protfolio!</h1>
+               <h1>Welcome to My Portfolio!</h1>
                  <img src={me} alt="me"/>
             </div>
               )}       
