e30583d8a9f6b61822936bf84889d286ad349770
jlangley3 <jlangleyiii@aol.com>
2020-12-23 20:20:45 -0500

    adding modal

--- src/components/About.js
+++ src/components/About.js
@@ -40,6 +40,21 @@ class About extends Component{
                 with clients with my new expertise in Ruby on Rails and JavaScript 
                 to a new opportunity as a developer.</p>
       </div>
+
+      {/* Trigger/Open The Modal */}
+<button id="myBtn">Open Modal</button>
+
+
+<div id="myModal" class="modal">
+
+  {/* Modal content  */}
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <p>Some text in the Modal..</p>
+  </div>
+
+</div>
+
       </div>
     
   )   
