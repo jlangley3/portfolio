2ca07713235bf2ccc2966a0e850f2c622f03cba4
jlangley3 <jlangleyiii@aol.com>
2020-12-30 23:40:11 -0500

    fixed pictures

--- src/components/Into.js
+++ src/components/Into.js
@@ -38,7 +38,7 @@ class Into extends Component {
   
      <a href={"https://www.mirror.co/shop/mirror?utm_source=google_ads&utm_medium=google_shopping&utm_campaign=mirror_shopping"}>  
      <img src="https://images.lululemon.com/is/image/lululemon/1_1_lg_Masonry_D%20Hero?$masonry_grid$&wid=2420&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72" 
-     alt="Italian Trulli" width="500px" height="600px"></img></a>
+     alt="Italian Trulli" width="900px" height="600px"></img></a>
  
 
                    <p>
