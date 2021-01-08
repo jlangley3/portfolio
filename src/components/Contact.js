e85fdb49ac530d4e1719876e2948522d4939833d
jlangley3 <jesselangleyiii@gmail.com>
2021-01-08 18:14:36 -0500

    adding css to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import { Link, withRouter } from "react-router-dom";
+import '../styles/Contact.css';
+
 
 class Contact extends Component{
 
@@ -7,10 +9,22 @@ class Contact extends Component{
        return (
    
       <div>
-            <p>Contact</p>
+            <p className="title">Contact</p>
+            <a href={"https://www.mirror.co/shop/mirror?utm_source=google_ads&utm_medium=google_shopping&utm_campaign=mirror_shopping"}>  
+     <img src="https://images.lululemon.com/is/image/lululemon/1_1_lg_Masonry_D%20Hero?$masonry_grid$&wid=2420&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72" 
+     alt="Italian Trulli" width="900px" height="600px"></img></a>
             <p>Linkedin</p>
+            <a href={"https://www.mirror.co/shop/mirror?utm_source=google_ads&utm_medium=google_shopping&utm_campaign=mirror_shopping"}>  
+     <img src="https://images.lululemon.com/is/image/lululemon/1_1_lg_Masonry_D%20Hero?$masonry_grid$&wid=2420&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72" 
+     alt="Italian Trulli" width="900px" height="600px"></img></a>
             <p>Email</p>
+            <a href={"https://www.mirror.co/shop/mirror?utm_source=google_ads&utm_medium=google_shopping&utm_campaign=mirror_shopping"}>  
+     <img src="https://images.lululemon.com/is/image/lululemon/1_1_lg_Masonry_D%20Hero?$masonry_grid$&wid=2420&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72" 
+     alt="Italian Trulli" width="900px" height="600px"></img></a>
             <p>Blog</p>
+            <a href={"https://www.mirror.co/shop/mirror?utm_source=google_ads&utm_medium=google_shopping&utm_campaign=mirror_shopping"}>  
+     <img src="https://images.lululemon.com/is/image/lululemon/1_1_lg_Masonry_D%20Hero?$masonry_grid$&wid=2420&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72" 
+     alt="Italian Trulli" width="900px" height="600px"></img></a>
 
 
       </div>
