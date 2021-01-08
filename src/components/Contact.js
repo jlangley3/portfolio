f1aaa718da25535cbf5227737cc3f107e4339fb0
jlangley3 <jesselangleyiii@gmail.com>
2021-01-08 18:17:23 -0500

    adding classnames

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,19 +9,20 @@ class Contact extends Component{
        return (
    
       <div>
-            <p className="title">Contact</p>
+          <Link as="a" activeClassName="active" to="/Contact">Contact Page</Link>
+            <p className="title">Contact Page</p>
             <a href={"https://www.mirror.co/shop/mirror?utm_source=google_ads&utm_medium=google_shopping&utm_campaign=mirror_shopping"}>  
      <img src="https://images.lululemon.com/is/image/lululemon/1_1_lg_Masonry_D%20Hero?$masonry_grid$&wid=2420&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72" 
      alt="Italian Trulli" width="900px" height="600px"></img></a>
-            <p>Linkedin</p>
+            <p className="title">Linkedin</p>
             <a href={"https://www.mirror.co/shop/mirror?utm_source=google_ads&utm_medium=google_shopping&utm_campaign=mirror_shopping"}>  
      <img src="https://images.lululemon.com/is/image/lululemon/1_1_lg_Masonry_D%20Hero?$masonry_grid$&wid=2420&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72" 
      alt="Italian Trulli" width="900px" height="600px"></img></a>
-            <p>Email</p>
+            <p className="title">Email</p>
             <a href={"https://www.mirror.co/shop/mirror?utm_source=google_ads&utm_medium=google_shopping&utm_campaign=mirror_shopping"}>  
      <img src="https://images.lululemon.com/is/image/lululemon/1_1_lg_Masonry_D%20Hero?$masonry_grid$&wid=2420&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72" 
      alt="Italian Trulli" width="900px" height="600px"></img></a>
-            <p>Blog</p>
+            <p className="title">Blog</p>
             <a href={"https://www.mirror.co/shop/mirror?utm_source=google_ads&utm_medium=google_shopping&utm_campaign=mirror_shopping"}>  
      <img src="https://images.lululemon.com/is/image/lululemon/1_1_lg_Masonry_D%20Hero?$masonry_grid$&wid=2420&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72" 
      alt="Italian Trulli" width="900px" height="600px"></img></a>
