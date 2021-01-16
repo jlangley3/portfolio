c69c6a2fb46333d5affa9163b6e1b382b35b7acc
jlangley3 <jesselangleyiii@gmail.com>
2021-01-15 20:28:46 -0500

    links working on

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { Link, withRouter } from "react-router-dom";
 import con from '../images/contact.jpg';
 import linkedin from '../images/Linkedin.jpg';
+import gmail from '../images/gmail.png';
 import '../styles/Contact.css';
 
 
@@ -14,12 +15,12 @@ class Contact extends Component{
          <p className="title">Contact Page</p> <Link as="a" activeClassName="active" to="/Contact"><img src={con} className="pic" /></Link>
             
          
-            <p className="title">Linkedin<Link as="a" activeClassName="active" to=""><img src={linkedin}  /></Link></p>
+            <p className="title">Linkedin</p>
             <a href={"https://www.linkedin.com/in/jesselangley"}>  
            <img src={linkedin} alt="Italian Trulli" width="900px" height="600px"></img></a>
             <p className="title">Email</p>
             <a href={"https://jesselangleyiii@gmail.com"}>  
-     <img src="https://images.lululemon.com/is/image/lululemon/1_1_lg_Masonry_D%20Hero?$masonry_grid$&wid=2420&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72" 
+     <img src={gmail} 
      alt="Italian Trulli" width="900px" height="600px"></img></a>
             <p className="title">Blog</p>
             <a href={"https://www.mirror.co/shop/mirror?utm_source=google_ads&utm_medium=google_shopping&utm_campaign=mirror_shopping"}>  
