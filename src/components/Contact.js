42d14496003f5055e9fa97156840e671082b7ca1
jlangley3 <jesselangleyiii@gmail.com>
2021-01-15 19:04:04 -0500

    reworking css for titles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,11 +10,9 @@ class Contact extends Component{
        return (
    
       <div>
-          <Link as="a" activeClassName="active" to="/Contact"><img src={con} className="githubIcon" /></Link>
-            <p className="title">Contact Page</p>
-            <a href={"https://www.mirror.co/shop/mirror?utm_source=google_ads&utm_medium=google_shopping&utm_campaign=mirror_shopping"}>  
-     <img src="https://images.lululemon.com/is/image/lululemon/1_1_lg_Masonry_D%20Hero?$masonry_grid$&wid=2420&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72" 
-     alt="Italian Trulli" width="900px" height="600px"></img></a>
+         <p className="title">Contact Page</p> <Link as="a" activeClassName="active" to="/Contact"><img src={con} className="githubIcon" /></Link>
+            
+   
             <p className="title">Linkedin</p>
             <a href={"https://www.mirror.co/shop/mirror?utm_source=google_ads&utm_medium=google_shopping&utm_campaign=mirror_shopping"}>  
      <img src="https://images.lululemon.com/is/image/lululemon/1_1_lg_Masonry_D%20Hero?$masonry_grid$&wid=2420&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72" 
