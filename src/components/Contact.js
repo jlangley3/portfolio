dcaffb34b2d1b621551af4020f2d4b8878fe21d6
jlangley3 <jesselangleyiii@gmail.com>
2021-01-15 20:32:07 -0500

    working on css for links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,6 +3,7 @@ import { Link, withRouter } from "react-router-dom";
 import con from '../images/contact.jpg';
 import linkedin from '../images/Linkedin.jpg';
 import gmail from '../images/gmail.png';
+import blog from '../images/whatisblog.png';
 import '../styles/Contact.css';
 
 
@@ -22,14 +23,11 @@ class Contact extends Component{
             <a href={"https://jesselangleyiii@gmail.com"}>  
      <img src={gmail} 
      alt="Italian Trulli" width="900px" height="600px"></img></a>
-            <p className="title">Blog</p>
-            <a href={"https://www.mirror.co/shop/mirror?utm_source=google_ads&utm_medium=google_shopping&utm_campaign=mirror_shopping"}>  
-     <img src="https://images.lululemon.com/is/image/lululemon/1_1_lg_Masonry_D%20Hero?$masonry_grid$&wid=2420&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72" 
-     alt="Italian Trulli" width="900px" height="600px"></img></a>
+
 
 <p className="title">Blog</p>
             <a href={"https://www.mirror.co/shop/mirror?utm_source=google_ads&utm_medium=google_shopping&utm_campaign=mirror_shopping"}>  
-     <img src="https://images.lululemon.com/is/image/lululemon/1_1_lg_Masonry_D%20Hero?$masonry_grid$&wid=2420&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72" 
+     <img src={blog}
      alt="Italian Trulli" width="900px" height="600px"></img></a>
 
 
