cacde6152675e108e7652fb1601656c3a2d522c4
jlangley3 <jesselangleyiii@gmail.com>
2021-01-15 20:07:44 -0500

    fixing link buttons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,8 +13,8 @@ class Contact extends Component{
       <div>
          <p className="title">Contact Page</p> <Link as="a" activeClassName="active" to="/Contact"><img src={con} className="pic" /></Link>
             
-         <Link as="a" activeClassName="active" to="/Contact"><img src={linkedin} className="title" /></Link>
-            <p className="title">Linkedin</p>
+         
+            <p className="title">Linkedin<Link as="a" activeClassName="active" to="www.linkedin.com/in/jesselangley"><img src={linkedin}  /></Link></p>
             <a href={"https://www.mirror.co/shop/mirror?utm_source=google_ads&utm_medium=google_shopping&utm_campaign=mirror_shopping"}>  
      <img src="https://images.lululemon.com/is/image/lululemon/1_1_lg_Masonry_D%20Hero?$masonry_grid$&wid=2420&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72" 
      alt="Italian Trulli" width="900px" height="600px"></img></a>
