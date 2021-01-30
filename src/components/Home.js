1e6c3d93333296fbd94923c448622fc16addef75
jlangley3 <jesselangleyiii@gmail.com>
2021-01-29 20:55:34 -0500

    adding button'

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link, withRouter } from "react-router-dom";
 import me from '../images/jessepic.jpg';
 import '../styles/Home.css';
 
@@ -10,11 +11,11 @@ class Home extends React.Component{
                <h1>Welcome to My Portfolio!</h1>
                  <img src={me} alt="me"/>
 
-                 <Link to="/dashboard"></Link>
-
+                 <Link to="/Projects">
                  <button type="button">
                   See my work
               </button>
+              </Link>
             </div>
               )}       
 }
