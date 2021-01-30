82e7a9fd1218286ae2a87013ed7f25590c284653
jlangley3 <jesselangleyiii@gmail.com>
2021-01-29 21:35:21 -0500

    button working

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,12 +10,12 @@ class Home extends React.Component{
             <div className="myPic">
                <h1>Welcome to My Portfolio!</h1>
                  <img src={me} alt="me"/>
-
+                 <div>
                  <Link to="/Projects">
                  <button className="button" type="button">
                   <p className="see">See my work</p>
               </button>
-              </Link>
+              </Link></div>
             </div>
               )}       
 }
