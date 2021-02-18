b5a51710862caf51262f28c119abb81d3abfd706
jlangley3 <jesselangleyiii@gmail.com>
2021-02-17 21:33:36 -0500

    adding to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,19 +13,20 @@ class Contact extends Component{
        return (
    
       <div className="contactpage">
-         <p className="title"><img src={linkedin} alt="Italian Trulli" width="900px" height="600px"></img>Contact Page</p> <Link as="a" activeClassName="active" to="/Contact"><img src={con} className="pic" /></Link>
-            
          
-            <p className="title">Linkedin</p>
+         {/* <Link as="a" activeClassName="active" to="/Contact"><img src={con} className="pic" /></Link> */}
+         <Link as="a" activeClassName="active" to="/Contact"><p className="title2">Click to Find Me!</p></Link>
+         
+            {/* <p className="title">Linkedin</p> */}
             <a href={"https://www.linkedin.com/in/jesselangley"}>  
            <img src={linkedin} alt="Italian Trulli" width="900px" height="600px"></img></a>
-            <p className="title">Email</p>
+            {/* <p className="title">Email</p> */}
             <a href={"https://jesselangleyiii@gmail.com"}>  
      <img src={gmail} 
      alt="Italian Trulli" width="900px" height="600px"></img></a>
 
 
-     <p className="title">Blog</p>
+     {/* <p className="title">Blog</p> */}
             <a href={"https://www.mirror.co/shop/mirror?utm_source=google_ads&utm_medium=google_shopping&utm_campaign=mirror_shopping"}>  
      <img src={blog}
      alt="Italian Trulli" width="900px" height="600px"></img></a>
